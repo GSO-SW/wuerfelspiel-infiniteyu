bb8b95af5f5ea0ae0a6e208235142817adcf795c
infinitec0des <48029692+infinitec0des@users.noreply.github.com>
2022-04-02 14:51:18 +0200

    Implemented First Unit Tests

--- WuerfelspielTests/WuerfelTests.cs
+++ WuerfelspielTests/WuerfelTests.cs
@@ -14,5 +14,62 @@ namespace WuerfelspielTests
             // Einfach löschen :)
             Assert.IsTrue(true);// is tatsächlich true :O
         }
+
+        [TestMethod]
+        public void WuerfelAnlegenErfolgreich()
+        {
+            Wuerfel wuerfel1;
+
+            wuerfel1 = new Wuerfel(6);
+
+            Assert.IsTrue(wuerfel1 != null);
+        }
+
+        [TestMethod]
+        public void AnzahlSeitenAbrufen()
+        {
+            Wuerfel wuerfel1;
+            int seitenAnzahl = 6;
+
+            wuerfel1 = new Wuerfel(seitenAnzahl);
+            Console.WriteLine(wuerfel1.AnzahlSeiten);
+
+            Assert.IsEqual(wuerfel1.AnzahlSeiten, seitenAnzahl);
+        }
+
+        [TestMethod]
+        public void WuerfelWuerfeln()
+        {
+            Wuerfel wuerfel1;
+            int seitenAnzahl = 6;
+
+            wuerfel1 = new Wuerfel(seitenAnzahl);
+            wuerfel1.wuerfeln();
+
+        }
+
+        [TestMethod]
+        public void LetztesErgebnisAbrufen()
+        {
+            Wuerfel wuerfel1;
+            int seitenAnzahl = 6;
+
+            wuerfel1 = new Wuerfel(seitenAnzahl);
+            wuerfel1.wuerfeln();
+
+            wuerfel1.LetztesErgebnis;
+        }
+
+        [TestMethod]
+        public void WuerfelSichern()
+        {
+            Wuerfel wuerfel1;
+            int seitenAnzahl = 6;
+
+            wuerfel1 = new Wuerfel(seitenAnzahl);
+            wuerfel1.wuerfeln();
+
+            wuerfel1.SicherungUmschalten();
+        }
     }
 }
